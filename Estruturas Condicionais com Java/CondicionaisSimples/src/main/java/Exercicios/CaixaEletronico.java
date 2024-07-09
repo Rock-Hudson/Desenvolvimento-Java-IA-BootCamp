1892d56034a8bf9a3de064b6533b86477b72e213
Rock Hudson <rockhudsonmillas@gmail.com>
2024-07-09 13:15:58 -0300

    Condicionais compostas

--- Estruturas Condicionais com Java/CondicionaisSimples/src/main/java/Exercicios/CaixaEletronico.java
+++ Estruturas Condicionais com Java/CondicionaisSimples/src/main/java/Exercicios/CaixaEletronico.java
@@ -5,11 +5,13 @@ public class CaixaEletronico {
     public static void main(String[] args) {
 
         double saldo = 220.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 230.0;
 
         if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
             System.out.println(saldo);
+        }else {
+            System.out.println("Salado Insuficiente!!!");
         }
 
 
